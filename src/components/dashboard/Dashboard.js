8c814ed564fcb7bd07da9435c562b3e6334b5bc0
Mahmudul Alam <expelmahmud@gmail.com>
2021-05-31 21:36:33 +0600

    add: User Table

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -3,6 +3,7 @@ import Navbar from '../layout/Navbar';
 import BarChart from './Charts/BarChart';
 import LiquidChart from './Charts/LiquidChart';
 import PieChart from './Charts/PieChart';
+import UserTable from './UserTable';
 
 export default function Dashboard() {
   return (
@@ -10,20 +11,28 @@ export default function Dashboard() {
       <Navbar />
 
       <div className="custom-padding">
-        <Row gutter={24}>
-          <Col md={8}>
+        {/* Charts */}
+        <Row gutter={[24, 16]}>
+          <Col md={8} sm={12} xs={24}>
             <Card title="Bar Chart" bordered={false}>
               <BarChart />
             </Card>
           </Col>
-          <Col md={8}>
+          <Col md={8} sm={12} xs={24}>
             <Card title="Pie Chart" bordered={false}>
               <PieChart />
             </Card>
           </Col>
-          <Col md={8}>
+          <Col md={8} sm={12} xs={24}>
             <Card title="Liquid Chart" bordered={false}>
               <LiquidChart />
+            </Card>
+          </Col>
+
+          {/* User Table */}
+          <Col xs={24}>
+            <Card title="User Table" bordered={false}>
+              <UserTable />
             </Card>
           </Col>
         </Row>
---
+++ src/components/dashboard/UserTable.js
@@ -0,0 +1,83 @@
+import { Table, Tag } from 'antd';
+
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+    title: 'Email',
+    dataIndex: 'email',
+    key: 'email',
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+  },
+  {
+    title: 'Sex',
+    dataIndex: 'sex',
+    key: 'sex',
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'address',
+  },
+  {
+    title: 'Tags',
+    key: 'tags',
+    dataIndex: 'tags',
+    render: (tags) => (
+      <>
+        {tags.map((tag) => {
+          let color = tag.length > 5 ? 'geekblue' : 'green';
+          if (tag === 'loser') {
+            color = 'volcano';
+          }
+          return (
+            <Tag color={color} key={tag}>
+              {tag.toUpperCase()}
+            </Tag>
+          );
+        })}
+      </>
+    ),
+  },
+];
+
+const data = [
+  {
+    key: '1',
+    name: 'John Brown',
+    age: 32,
+    sex: 'Male',
+    email: 'jbrown@gmail.com',
+    address: 'New York No. 1 Lake Park',
+    tags: ['nice', 'developer'],
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+    age: 42,
+    sex: 'Male',
+    email: 'jgreen@gmail.com',
+    address: 'London No. 1 Lake Park',
+    tags: ['loser'],
+  },
+  {
+    key: '3',
+    name: 'Joe Black',
+    age: 32,
+    sex: 'Male',
+    email: 'jblack@gmail.com',
+    address: 'Sidney No. 1 Lake Park',
+    tags: ['cool', 'teacher'],
+  },
+];
+
+export default function UserTable() {
+  return <Table columns={columns} dataSource={data} />;
+}
